357ec19c992b7cc7be203e0d81577b6fd25d0650
VienThong1609 <2051012111thong@ou.edu.vn>
2022-10-26 00:24:32 +0700

    Update form

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    partial class Form1
+    partial class TinhToan
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TinhToan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "TinhToan";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,8 +1,8 @@
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class TinhToan : Form
     {
-        public Form1()
+        public TinhToan()
         {
             InitializeComponent();
         }
